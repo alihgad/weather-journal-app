e832c46b6284d0117924516238753e13a62e771e
alihgad <alihgad2@gmail.com>
2023-01-10 22:53:25 +0200

    maid

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {name:'ali' , Age:23 };
+projectData = { };
 
 // Require Express to run server and routes
 const express = require('express')
--- website/app.js
+++ website/app.js
@@ -1,6 +1,29 @@
+
 /* Global Variables */
+const generate = document.querySelector('#generate');
+const zip = document.querySelector('#zip');
+const feelings = document.querySelector('#feelings');
+const feeling = document.querySelector('.feelings');
+const temp = document.querySelector('#temp');
 // Personal API Key for OpenWeatherMap API
-const apiKey = '6641be4658cdae05e1628444aba67ac8&units=imperial';
+const apiKey = '&appid=f02311ef44f6f32cff2a21d054c3333f&units=imperial';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const d = new Date();
+const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+generate.addEventListener('click', (event) => {
+    event.preventDefault();
+    const madeURL = `${baseURL}${zip.value}${apiKey}`;
+    getData(madeURL);
+});
+
+const getData = async(URL) => {
+        try{
+            const result = await fetch (URL);
+            const data = await result.json();
+            console.log(data)
+            return data;
+        }catch(e){
+            console.log(`error${e}`)
+        }
+}
